02906d2db2d89992368b617d5555fe8796dfffc3
Ronen Atias <ronen_at@yahoo.com>
2023-08-13 09:39:54 +0300

    Update testing.sh

--- testing.sh
+++ testing.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 kubectl proxy --accept-hosts '.*' &
 docker run -it -p 8080:8080 -e CLUSTERS=http://docker.for.mac.localhost:8001 hjacobs/kube-ops-view
+
+
+kubectl proxy --address='0.0.0.0' --port=8002 --accept-hosts='.*'
